53cd0252ca8ab5bfdda7daea45b9ecef590a2587
Sanskriti singh <146508510+Sanskriti3104@users.noreply.github.com>
2025-08-03 23:19:49 +0530

    Implement console version of addToLibrary and track stats

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-console.log("Welcome to the Library");
-
 const myLibrary = [];
 
 function Book(title, author, pages, hasRead, uniqueId) {
-    if (!new.target) {
+    if(!new.target){
         throw Error("You must use the 'new' operator to call the constructor");
     }
 
@@ -14,25 +12,32 @@ function Book(title, author, pages, hasRead, uniqueId) {
     this.uniqueId = uniqueId;
 }
 
-function addBookToLibrary() {
+const submitButton = document.querySelector(".submit-btn");
+const totalBooks = document.getElementById("totalBooks");
+const booksRead = document.getElementById("booksRead");
 
-    while (true) {
-        let title = prompt("Enter Book Name :");
-        let author = prompt("Enter Author's Name: ");
-        let pages = prompt("Enter number of pages: ");
-        let hasRead = confirm("Have you read this book?");
+submitButton.addEventListener("click", addBookToLibrary);
 
-        let uniqueId = crypto.randomUUID();
+function addBookToLibrary(event) {
+    event.preventDefault();
+    const bookTitle = document.getElementById("book-title").value;
+    const author = document.getElementById("author").value;
+    const pageCount = document.getElementById("page-count").value;
+    const readStatus = document.getElementById("read-status").checked;
 
-        let newBook = new Book(title, author, pages, hasRead, uniqueId);
-        myLibrary.push(newBook);
+    let uniqueId = crypto.randomUUID();
 
-        if (!confirm("Do you want to add another book?")) break;
-    }
+    let newBook = new Book(bookTitle, author, pageCount, readStatus, uniqueId);
+    myLibrary.push(newBook);
 
+    let reads = 0;
     for (let i = 0; i < myLibrary.length; i++) {
         console.log(myLibrary[i]);
+        if (myLibrary[i].hasRead) {
+            reads++;
+        }
     }
-}
 
-addBookToLibrary();
+    totalBooks.textContent = myLibrary.length;
+    booksRead.textContent = reads;
+}
